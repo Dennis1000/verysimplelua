288cb922af0666b43ad00e2d570d3fc2703eae4b
Zaher Dirkey <zaherdirkey@yahoo.com>
2018-05-27 23:42:04 +0300

    fix send error when load string/file

--- Source/VerySimple.Lua.pas
+++ Source/VerySimple.Lua.pas
@@ -809,7 +809,8 @@ begin
   if not Opened then
     Open;
   Result := lual_loadfile(LuaState, Marshall.AsAnsi(Filename).ToPointer);
-  DoError(UTF8ToString(lua_tostring(LuaState, -1)));
+  if Result <> 0 then
+    DoError(UTF8ToString(lua_tostring(LuaState, -1)));
 end;
 
 
@@ -1019,7 +1020,8 @@ begin
   if not Opened then
     Open;
   Result := luaL_loadstring(LuaState, Marshall.AsAnsi(Value).ToPointer);
-  DoError(UTF8ToString(lua_tostring(LuaState, -1)));
+  if Result <> 0 then
+    DoError(UTF8ToString(lua_tostring(LuaState, -1)));
 end;
 
 class function TVerySimpleLua.LuaLibraryLoaded: Boolean;
